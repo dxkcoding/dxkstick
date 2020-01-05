fab7a776c4ad4943974697cc9b356ad9db7b4dc3
陈斌 <chbpku@gmail.com>
2020-01-05 09:18:32 +0800

    更新。

--- main.ts
+++ main.ts
@@ -1,4 +1,8 @@
-//% weight=0 color=#94070A icon="" block="dxktest"
+//% weight=0 color=#94070A icon="\uf57e" block="dxktest"
 namespace dxktest {
-    //% blockId="humidity" block="get humidity"
+    //% blockId="humidity" block="Get Humidity slot %slot"
+    export function getHumidity(slot: number): number
+    {
+        return slot
+    }
 } 

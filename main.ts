eaf76ac7d1d987d8ca5287c3aec69928150bef0a
陈斌 <chbpku@gmail.com>
2020-01-05 10:38:37 +0800

    更新。

--- main.ts
+++ main.ts
@@ -4,11 +4,26 @@ enum Slot {
     //% block="B"
     SlotB = 0x17
 }
+
+function bufferFromString(s: string):Buffer {
+    let buf = pins.createBuffer(s.length);
+    for (let i = 0; i< s.length; i++) {
+        buf.setNumber(NumberFormat.Int8LE, i, s.charCodeAt(i));
+    }
+    return buf;
+}
+
+function execCmd(slot: Slot, cmd: string): number {
+    pins.i2cWriteBuffer(slot, bufferFromString(cmd), false);
+    let val = pins.i2cReadNumber(slot, NumberFormat.Int8LE);
+    basic.pause(50);
+    return val;
+}
 //% weight=0 color=#94070A icon="\uf57e" block="dxktest"
 namespace dxktest {
-    //% blockId="humidity" block="Get Humidity slot %slot"
-    export function getHumidity(slot: Slot): number
-    {
-        return slot
+    //% blockId="humidity" block="Get Humidity from slot %slot"
+    export function getHumidity(slot: Slot): number {
+        //let cmd = pins.createBufferFromArray(Array "get_humi")
+        return execCmd(slot, "get_humi");
     }
 } 

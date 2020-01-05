d4aebbe88d0d4c3efe2561d1213a18cfec5e256d
陈斌 <chbpku@gmail.com>
2020-01-05 15:15:28 +0800

    更新。

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 enum Slot {
-    //% block="A"
+    //% block="Slot A"
     SlotA = 0x16,
-    //% block="B"
+    //% block="Slot B"
     SlotB = 0x17
 }
 
@@ -14,31 +14,41 @@ function bufferFromString(s: string): Buffer {
 }
 function execCmd(slot: Slot, cmd: string): void {
     pins.i2cWriteBuffer(slot, bufferFromString(cmd), false);
-    basic.pause(20);
+    //basic.pause(20);
     return;
 }
 function execCmdReturn(slot: Slot, cmd: string): number {
     pins.i2cWriteBuffer(slot, bufferFromString(cmd), false);
     let val = pins.i2cReadNumber(slot, NumberFormat.Int8LE);
-    basic.pause(50);
+    //basic.pause(50);
     return val;
 }
 //% weight=0 color=#94070A icon="\uf0ac" block="dxktest"
 namespace dxktest {
-    //% blockId="humidity" block="Get Humidity from slot %slot"
+    //% blockId="humidity" block="2Get Humidity from %slot"
     export function getHumidity(slot: Slot): number {
         return execCmdReturn(slot, "get_humi");
     }
-    //% blockId="temperature" block="Get Temperature from slot %slot"
+    //% blockId="temperature" block="Get Temperature from %slot"
     export function getTemp(slot: Slot): number {
         return execCmdReturn(slot, "get_temp");
     }
-    //% blockId="led_on" block="LED ON for slot %slot"
+    //% blockId="led_on" block="LED ON for %slot"
     export function ledON(slot: Slot): void {
-        execCmd(slot, "set_led_on")
+        execCmd(slot, "set_led_on");
     }
-    //% blockId="led_off" block="LED OFF for slot %slot"
+    //% blockId="led_off" block="LED OFF for %slot"
     export function ledOFF(slot: Slot): void {
-        execCmd(slot, "set_led_off")
+        execCmd(slot, "set_led_off");
+    }
+    //% blockId="oled_clear" block="OLED clear for %slot"
+    export function oledClearScreen(slot: Slot): void {
+        execCmd(slot, "ClearScreen");
+        basic.pause(10);
+    }
+    //% blockId="oled_show" block="OLED show message %msg| for %slot"
+    export function oledShowMsg(msg: string, slot: Slot) {
+        execCmd(slot, "DisplayGB2312,0,0,"+msg.substr(0,16));
+        basic.pause(15);
     }
 } 

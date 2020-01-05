04d412ec2ebe176f90bd9a2448ffc56f1ad8759f
陈斌 <chbpku@gmail.com>
2020-01-05 09:31:03 +0800

    更新。

--- main.ts
+++ main.ts
@@ -1,7 +1,13 @@
+enum Slot {
+    //% block="A"
+    SlotA = 0x16,
+    //% block="B"
+    SlotB = 0x17
+}
 //% weight=0 color=#94070A icon="\uf57e" block="dxktest"
 namespace dxktest {
     //% blockId="humidity" block="Get Humidity slot %slot"
-    export function getHumidity(slot: number): number
+    export function getHumidity(slot: Slot): number
     {
         return slot
     }

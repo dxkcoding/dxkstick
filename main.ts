00ecffb6efac3f6e27cc40c921a319f42047fa4d
dxkcoding <62132219+dxkcoding@users.noreply.github.com>
2021-02-26 10:24:26 +0800

    更新 pxt.json, main.ts, _locales/zh/dxktest-strings.json

--- main.ts
+++ main.ts
@@ -66,19 +66,21 @@ enum HandleAxis {
 }
 
 enum Neo_grps {
-    //% block="Ring_24"
-    Ring24 = 1,
-    //% block="Array_99"
-    Array99 = 2,
-    //% block="Belt_30"
-    Belt30 = 3
+    //% block="Ring"
+    Ring = 0x1,
+    //% block="Array"
+    Array = 0x2,
+    //% block="Belt"
+    Belt = 0x3
 }
 
 enum Animation_Type {
-    //% block="Rainbaw"
-    Rainbaw = 0,
-    //% block="Shinelon"
-    Shinelon = 1
+    //% block="Rainbow"
+    Rainbow = 0x1,
+    //% block="Chameleon"
+    Chameleon = 0x2,
+    //% block="Confetti"
+    Confetti = 0x3
 }
 
 function bufferFromString(s: string): Buffer {
@@ -128,47 +130,9 @@ function execCmdHandle(slot: Slot, ChosenByte: number): number {
     return val;
 }
 
-//% weight=0 color=#f58f98 icon="\uf0ac" block="dxktest"
+//% weight=0 color=#f58f98 icon="\uf0ac" block="DinoStick"
 namespace dxktest {
-    //% blockId="Temp_Humi" block="Get %temp_humi from %slot"
-    export function getTempHumi(slot: Slot, temp_humi: TempHumi): number {
-
-        switch (temp_humi) {
-            case 0x1: return execCmdReturn(slot, "get_temp");
-            case 0x2: return execCmdReturn(slot, "get_humi");
-            default: return 2;
-        }
-    }
-    //% blockId="potential_value" block="Get Potential Value from %slot"
-    export function getPoten(slot: Slot): number {
-        return execCmdReturn16(slot, "get_poten_val");
-    }
-    //% blockId="light_value" block="Get Light Value from %slot"
-    export function getLight(slot: Slot): number {
-        return execCmdReturn16(slot, "get_light_val");
-    }
-    //% blockId="Microphone_value" block="Get Microphone Value from %slot"
-    export function getMicrophone(slot: Slot): number {
-        return execCmdReturn16(slot, "get_mic_val");
-    }
-    //% blockId="Handle_Button_State" block="Get Handle %handle_button State from %slot"
-    export function getHandleButton(slot: Slot, handle_button: HandleButton): boolean {
-        if (execCmdHandle(slot, handle_button) > 0) {
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
-    //% blockId="Handle_Axis" block="Get %handle_axis from %slot"
-    export function getHandleAxis(slot: Slot, handle_axis: HandleAxis): number {
-        return execCmdHandle(slot, handle_axis);
-    }
-    //% blockId="Distance_Value" block="Get Distance Value from %slot"
-    export function getDistance(slot: Slot): number {
-        return execCmdReturn16(slot, "get_distance_val");
-    }
-    //% blockId="BMP_Data" block="Get %bmpdatatype from %slot"
+    /*//% blockId="BMP_Data" block="Get %bmpdatatype from %slot"
     export function getBMPData(slot: Slot, bmpdatatype: BMPDataType): number {
         switch (bmpdatatype) {
             case 0x1: return execCmdReturn16(slot, "getP");
@@ -176,27 +140,13 @@ namespace dxktest {
             case 0x3: return execCmdReturn16(slot, "getA");
             default: return 1;
         }
-    }
-    //% blockId="Touch_State" block="Get Touch State from %slot"
-    export function getTouchState(slot: Slot): boolean {
-        return execCmdReturnBool(slot, "get_touch");
-    }
-    //% blockId="Led_ON" block="Set LED ON in %slot"
-    export function LedON(slot: Slot): void {
-        execCmd(slot, "set_led_on");
-    }
-    //% blockId="Led_OFF" block="Set LED OFF in %slot"
-    export function LedOFF(slot: Slot): void {
-        execCmd(slot, "set_led_off");
-    }
-    //% blockId="set_time" block="Set Time in %slot as "
-    export function setTime(slot: Slot, year: number, month: number, day: number, h: number, m: number, s: number): void {
-        execCmd(slot, "setT" + String.fromCharCode(year) + String.fromCharCode(month) + String.fromCharCode(day) + String.fromCharCode(h) + String.fromCharCode(m) + String.fromCharCode(s));
-    }
+    }*/
+
     //% blockId="get_time" block="Get %datetimeoption in %slot as "
     export function getTime(slot: Slot, datetimeoption: DateTimeOption): string {
         pins.i2cWriteBuffer(slot, bufferFromString("getT"), false);
         let buf = pins.i2cReadBuffer(slot, 6);
+        return buf.getNumber(NumberFormat.Int8BE , 0).toString();
         let yearnumber = buf.getNumber(NumberFormat.Int8BE, 0);
         let year = '';
         if (yearnumber > 10) {
@@ -231,7 +181,22 @@ namespace dxktest {
             default: return(buf.getNumber(NumberFormat.Int8BE, 0) - 1);
         }
     }    
+
+    //% blockId="set_time" block="Set Time in %slot as %year Year %month Month %day Day %h Hour %m Minute %s Second"
+    //% inlineInputMode=inline
+    //% year.min=0 year.max=99 month.min=1 month.max=12 day.min=1 day.max=31 h.min=0 h.max=23 m.min=0 m.max=59 s.min=0 s.max=59
+    export function setTime(slot: Slot, year: number, month: number, day: number, h: number, m: number, s: number): void {
+        execCmd(slot, "setT" + String.fromCharCode(year) + String.fromCharCode(month) + String.fromCharCode(day) + String.fromCharCode(h) + String.fromCharCode(m) + String.fromCharCode(s));
+    }
+
+    //% blockId="steering_engine" block="Move steering_engine in %slot to Angle %speed"
+    //% speed.min=0 speed.max=270
+    export function SetSteeringEngine(slot: Slot, angle: number): void {
+        execCmd(slot, 'conA' + String.fromCharCode(Math.floor(angle/1.5)));
+        //basic.pause(1000);
+    }
     //% blockId="motor" block="Move Motor in %slot at Speed %speed"
+    //% speed.min=-1023 speed.max=1023
     export function Motor(slot: Slot, speed: number): void {
         if (speed > 0) {
             execCmd(slot, 'getf' + String.fromCharCode(Math.abs(speed)));
@@ -241,35 +206,81 @@ namespace dxktest {
         }
         //basic.pause(1000);
     }
-    //% blockId="steering_engine" block="Move steering_engine in %slot to Angle %speed"
-    export function SetSteeringEngine(slot: Slot, angle: number): void {
-        execCmd(slot, 'conA' + String.fromCharCode(Math.abs(angle)));
-        //basic.pause(1000);
+    //% blockId="Touch_State" block="Get Touch State from %slot"
+    export function getTouchState(slot: Slot): boolean {
+        return execCmdReturnBool(slot, "get_touch");
     }
-    //% blockId="setup_neo" block="Setup Neo in %slot"
-    export function setupNeo(slot: Slot, neo_grps: Neo_grps): void {
-        execCmd(slot, "init1" + String.fromCharCode(neo_grps));
-        basic.pause(10);
-    }
-    //% blockId="set_neo_pixel_color" block="Set Neo NO.%pos Pixel's Color to (r:%r ,g:%g ,b:%b ) in %slot"
-    export function setNeoPixelColor(slot: Slot, pos: number, r: number, g: number, b: number): void {
-        execCmd(slot, "setP" + String.fromCharCode(0) + String.fromCharCode(pos) + String.fromCharCode(r) + String.fromCharCode(g) + String.fromCharCode(b));
-        basic.pause(10);
-    }
-    //% blockId="set_neo_array99_color" block="Set Neo Array99 Pixel's Color at (X:%x ,Y:%y ) to (r:%r ,g:%g ,b:%b ) in %slot"
-    export function setNeoArray99Color(slot: Slot, x: number, y: number, r: number, g: number, b: number): void {
-        execCmd(slot, "setX" + String.fromCharCode(0) + String.fromCharCode(x) + String.fromCharCode(y) + String.fromCharCode(r) + String.fromCharCode(g) + String.fromCharCode(b));
+
+    //% blockId="set_neo_rainbow" block="Set Neo Rainbow %t at Position %x with Lenth %n in %slot"
+    //% inlineInputMode=inline
+    export function setNeoRainbow(t: Animation_Type, x: number, n: number, slot: Slot ): void {
+        switch (t){
+            case 0x1 : 
+                execCmd(slot, "rnbl" + String.fromCharCode(0) + "N" + String.fromCharCode(x) + String.fromCharCode(n));
+                break;
+            case 0x2 : 
+                execCmd(slot, "rnbl" + String.fromCharCode(0) + "H" + String.fromCharCode(x) + String.fromCharCode(n));
+                break;
+            case 0x3 : 
+                execCmd(slot, "rnbl" + String.fromCharCode(0) + "O" + String.fromCharCode(x) + String.fromCharCode(n));
+                break;
+        }
         basic.pause(10);
     }
     //% blockId="fill_neo_color" block="Fill Neo with Color of (r:%r ,g:%g ,b:%b ) in %slot"
-    export function fillNeoColor(slot: Slot, r: number, g: number, b: number): void {
+    //% r.min=0 r.max=255 g.min=0 g.max=255 b.min=0 b.max=255  
+    //% inlineInputMode=inline
+    export function fillNeoColor(r: number, g: number, b: number, slot: Slot): void {
         execCmd(slot, "fill" + String.fromCharCode(0) + String.fromCharCode(r) + String.fromCharCode(g) + String.fromCharCode(b));
         basic.pause(10);
     }
-    //% blockId="set_neo_rainbpow" block="Set Neo Rainbow %t at Position %x with Lenth %n in %slot"
-    export function setNeoRainbow(slot: Slot, t: Animation_Type, x: number, n: number): void {
-        execCmd(slot, "rnbl" + String.fromCharCode(0) + String.fromCharCode(t) + String.fromCharCode(x) + String.fromCharCode(n));
+    //% blockId="set_neo_array_color" block="Set Neo Array Pixel's Color at (X:%x ,Y:%y ) to (r:%r ,g:%g ,b:%b ) in %slot"
+    //% r.min=0 r.max=255 g.min=0 g.max=255 b.min=0 b.max=255 X.min=0 X.max=5 Y.min=0 Y.max=5 
+    //% inlineInputMode=inline
+    export function setNeoArrayColor(x: number, y: number, r: number, g: number, b: number, slot: Slot): void {
+        execCmd(slot, "setX" + String.fromCharCode(0) + String.fromCharCode(x) + String.fromCharCode(y) + String.fromCharCode(r) + String.fromCharCode(g) + String.fromCharCode(b));
         basic.pause(10);
+    }
+    //% blockId="set_neo_pixel_color" block="Set Neo NO.%pos Pixel's Color to |[r:%r ,g:%g ,b:%b] in %slot"
+    //% r.min=0 r.max=255 g.min=0 g.max=255 b.min=0 b.max=255
+    //% inlineInputMode=inline
+    export function setNeoPixelColor(pos: number, r: number, g: number, b: number, slot: Slot): void {
+        execCmd(slot, "setP" + String.fromCharCode(0) + String.fromCharCode(pos) + String.fromCharCode(r) + String.fromCharCode(g) + String.fromCharCode(b));
+        basic.pause(10);
+    }
+    //% blockId="setup_neo" block="Setup Neo %Neo_grps in %slot "
+    export function setupNeo(neo_grps: Neo_grps, slot: Slot): void {
+        switch(neo_grps){
+            case 0x1 : 
+                execCmd(slot, "init1" + "R");
+                break;
+            case 0x2 : 
+                execCmd(slot, "init1" + "A");
+                break;
+            case 0x3 : 
+                execCmd(slot, "init1" + "B");
+                break;
+        }
+        basic.pause(10);
+    }
+
+    
+    //% blockId="Handle_Button_State" block="Get Handle %handle_button State from %slot"
+    export function getHandleButton(slot: Slot, handle_button: HandleButton): boolean {
+        if (execCmdHandle(slot, handle_button) > 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    //% blockId="Handle_Axis" block="Get %handle_axis from %slot"
+    export function getHandleAxis(slot: Slot, handle_axis: HandleAxis): number {
+        return execCmdHandle(slot, handle_axis);
+    }
+    //% blockId="Distance_Value" block="Get Distance Value from %slot"
+    export function getDistance(slot: Slot): number {
+        return execCmdReturn16(slot, "get_distance_val");
     }
     //% blockId="oled_clear" block="OLED in %slot clear screen"
     export function oledClearScreen(slot: Slot): void {
@@ -284,5 +295,40 @@ namespace dxktest {
             }
         }
         //basic.pause(15);
+    }
+    //% blockId="Temp_Humi" block="Get %temp_humi from %slot"
+    export function getTempHumi(temp_humi: TempHumi, slot: Slot): number {
+        switch (temp_humi) {
+            case 0x1: return execCmdReturn(slot, "get_temp");
+            case 0x2: return execCmdReturn(slot, "get_humi");
+            default: return 2;
+        }
+    }
+    //% blockId="light_value" block="Get Light Value from %slot"
+    export function getLight(slot: Slot): number {
+        return execCmdReturn16(slot, "get_light_val");
+    }
+    basic.showNumber(0)
+    //% blockId="Microphone_value" block="Get Microphone Value from %slot"
+    export function getMicrophone(slot: Slot): number {
+        return execCmdReturn16(slot, "get_mic_val");
+    }
+    //% blockId="potential_value" block="Get Potential Value from %slot"
+    export function getPoten(slot: Slot): number {
+        return execCmdReturn16(slot, "get_poten_val");
+    }
+    //% blockID="LedColors"  block="Set LED Color in %slot with R %r  G %g  B %b "
+    //% r.min=0 r.max=255 g.min=0 g.max=255 b.min=0 b.max=255
+    //% inlineInputMode=inline
+    export function LedColor(slot: Slot, r: number, g: number, b: number):void{
+        execCmd(slot, "setC" + String.fromCharCode(r) + String.fromCharCode(g) + String.fromCharCode(b))
+    }
+    //% blockId="LedOFF" block="Set LED OFF in %slot"
+    export function LedOFF(slot: Slot): void {
+        execCmd(slot, "set_led_off");
+    }
+    //% blockId="LedON" block="Set LED ON in %slot"
+    export function LedON(slot: Slot): void {
+        execCmd(slot, "set_led_on");
     }
 } 
